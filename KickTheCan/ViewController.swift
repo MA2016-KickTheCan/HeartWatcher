78fa5054648381019379531a84070502120e0957
natmark <natmark0918@gmail.com>
2016-12-01 21:01:34 +0900

    [update] demon mode & change heartbeat sound

--- KickTheCan/ViewController.swift
+++ KickTheCan/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, MIOControllDelegate {
     @IBOutlet weak var debugLeftButton: UIButton!
     @IBOutlet weak var debugRightButton: UIButton!
     @IBOutlet weak var heartrateLabel: UILabel!
+    @IBOutlet weak var spheroButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +28,17 @@ class ViewController: UIViewController, MIOControllDelegate {
         self.view.addSubview(heartView)
 
         heartrateLabel.text = ""
+        spheroButton.isHidden = true
 
         do {
             // 音楽ファイルが"sample.mp3"の場合
-            let filePath = Bundle.main.path(forResource: "hito_ge_shinzo06", ofType: "mp3")
+            let filePath = Bundle.main.path(forResource: "heartbeat", ofType: "mp4")
             let audioPath = NSURL(fileURLWithPath: filePath!)
             audioPlayer = try AVAudioPlayer(contentsOf: audioPath as URL)
             audioPlayer.prepareToPlay()
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.volume = 0
+            audioPlayer.play()
         } catch {
             print("Error")
         }
@@ -41,34 +46,51 @@ class ViewController: UIViewController, MIOControllDelegate {
         if UserModel.sharedInstance.debugMode(flg: nil) {
             debugLeftButton.isHidden = false
             debugRightButton.isHidden = false
-            
-            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
-                self.trackingHeartrate(heartrate: self.debug_heartrate)
-            })
+
+            let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.sendHeartrate), userInfo: nil, repeats: true)
+            timer.fire()
         }else{
             DeviceModel.sharedInstance.delegate = self
             DeviceModel.sharedInstance.startTracking()
         }
+        if UserModel.sharedInstance.getUserRole() == RoleType.Demon {
+            spheroButton.isHidden = false
+            heartrateLabel.isHidden = true
+            heartView.isHidden = true
+            debugLeftButton.isHidden = true
+            debugRightButton.isHidden = true
+        }
+    }
+    @IBAction func spheroMover(_ sender: Any) {
+        DeviceModel.sharedInstance.moveSphero(angle:Int(arc4random() % 360) , speed: 0.5) //スフィロの移動
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        audioPlayer.stop()
+    }
+    func sendHeartrate(){
+        self.trackingHeartrate(heartrate: self.debug_heartrate)
     }
     func trackingHeartrate(heartrate: Int) {
         heartView.heart = heartrate
         heartrateLabel.text = "\(heartrate)"
         if(heartrate >= 90 && isExciting == false){
+            self.view.backgroundColor = UIColor.red;
             isExciting = true
-            audioPlayer.play()
-            audioPlayer.volume = 0.1
+            audioPlayer.volume = 0.3
             DeviceModel.sharedInstance.moveSphero(angle:Int(arc4random() % 360) , speed: 0.5) //スフィロの移動
             let colorCode = ["FF0000","00FF00","0000FF"]
             let colorNumber:Int = Int(arc4random() % UInt32(colorCode.count))
             DeviceModel.sharedInstance.lightSphero(colorCode:colorCode[colorNumber] , brightness:1.0) //スフィロの色
         }
         if(heartrate < 90 && isExciting == true){
+            self.view.backgroundColor = UIColor(red: 26/255.0, green: 26/255.0, blue: 26/255.0, alpha: 1.0)
+
             isExciting = false
-            audioPlayer.stop()
+            audioPlayer.volume = 0
         }
         
         if(isExciting){
-            audioPlayer.volume = min(Float((heartrate - 90) / 30),1.0)
+            audioPlayer.volume = min(Float((heartrate - 90) / 30) + 0.3,1.0)
         }
     }
     @IBAction func upperHeartrate(_ sender: Any) {
